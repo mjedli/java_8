e3a0b7e33465619e4c5de3e62b526e154bdd54bc
mjedli <jedli.mejdi.recup@gmail.com>
2019-09-10 17:19:45 +0200

    java 8

--- StringJava8.java
+++ StringJava8.java
@@ -1,6 +1,5 @@
-/**
- * 
- */
+import java.util.StringJoiner;
+import java.util.stream.IntStream;
 
 /**
  * @author mjedli
@@ -12,8 +11,29 @@ public class StringJava8 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Stream
 
+		String hello = "hello!";
+		
+		IntStream stream = hello.chars();
+		
+		stream.forEach(System.out::println);
+
+		// StringJoiner
+		
+		StringJoiner sj = new StringJoiner(":").add("20").add("22");
+		System.out.println(sj.toString()); // 20:22
+		
+		StringJoiner sj1 = new StringJoiner(":", "[" , "]").add("20").add("22");
+		System.out.println(sj1.toString()); // [20:22]
+		
+		String s = String.join(":", "20", "22");
+		System.out.println(s); // 20:22
+		
+		String [] tab = {"20", "22"};
+		String out = String.join(":", tab);
+		System.out.println(out); // 20:22
 	}
 
 }
