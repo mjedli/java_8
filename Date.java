6e8c4c66902fabe0d250f4cbfafc4bb91b6f72e0
mjedli <jedli.mejdi.recup@gmail.com>
2019-09-10 15:24:41 +0200

    LocalDate

--- Date.java
+++ Date.java
@@ -1,5 +1,7 @@
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Month;
 
 
 /**
@@ -12,21 +14,29 @@ public class Date {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 		Instant instant1 = Instant.now();
+		
 		try {
 			Thread.sleep(500);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
+		
 		Instant instant2 = Instant.now();
 		
 		Duration duration = Duration.between(instant1, instant2);
 		
 		System.out.println(duration.toMillis());
+
+		LocalDate now = LocalDate.now();
 		
+		System.out.println(now);
+		
+		LocalDate now1 = LocalDate.of(1900, Month.DECEMBER, 2);
+		
+		System.out.println(now1);
+		
+		System.out.println(now1.until(now).getYears() + "years");
 		
 	}
 
